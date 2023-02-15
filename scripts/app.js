59a0a93c3347fd7f5a3ea99104bb08477646bd47
JeremyLapham <jeremylapham2004@gmail.com>
2023-02-15 14:28:54 -0800

    completed didn't find bugs

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,74 @@
 import { saveToLocalStorage, getLocalStoage, removeFromLocalStorage } from "./localstorage.js";
+import { GroupsOfFive, GroupsOfFour, GroupsOfThree, GroupsOfTwo } from "./groups.js";
 
 const enterName = document.getElementById('enterName');
 const enterNameBtn = document.getElementById('enterNameBtn');
 const displayNames = document.getElementById('displayNames');
+const nameLengthDisplay = document.getElementById('nameLengthDisplay');
 
+const group2 = document.getElementById('group2');
+const group3 = document.getElementById('group3');
+const group4 = document.getElementById('group4');
+const group5 = document.getElementById('group5');
 
 enterName.addEventListener('keydown', function (e) {
-    if(e.key==='Enter'){
+    let letters = /^[A-Za-z-]+$/;
+    if (e.key === 'Enter') {
+        if (enterName.value == '' || !enterName.value.match(letters)) {
+            alert('Please enter a valid')
+        } else {
+            saveToLocalStorage(enterName.value)
+            displayNamesOnScreen()
+            enterName.value = '';
+        }
+    }
+});
+enterNameBtn.addEventListener('click', function () {
+    let letters = /^[A-Za-z-]+$/;
+    if (enterName.value == '' || !enterName.value.match(letters)) {
+        alert('Please enter a valid')
+    } else {
         saveToLocalStorage(enterName.value)
         displayNamesOnScreen()
         enterName.value = '';
     }
 });
-enterNameBtn.addEventListener('click', function () {
-    saveToLocalStorage(enterName.value)
-    displayNamesOnScreen()
-    enterName.value = '';
+
+group2.addEventListener('click', function () {
+    GroupsOfTwo()
+});
+group3.addEventListener('click', function () {
+    GroupsOfThree()
+});
+group4.addEventListener('click', function () {
+    GroupsOfFour()
+});
+group5.addEventListener('click', function () {
+    GroupsOfFive()
 });
 
 function displayNamesOnScreen() {
     displayNames.innerHTML = '';
-    let peopleNames = getLocalStoage()
-    peopleNames.map(name=> {
-        let row = document.createElement('row');
+    let peopleNames = getLocalStoage();
+    nameLength()
+    peopleNames.map(name => {
+        let row = document.createElement('div');
+        row.className = 'row';
         row.style.display = 'flex';
-        row.style.justifyContent = 'center';
         row.style.marginBottom = '1rem';
+        row.style.marginLeft = '1rem';
+        row.style.marginRight = '1rem';
+        row.style.border = '2px solid';
 
+        let col1 = document.createElement('div');
+        col1.className = 'col-6';
+        
+        let col2 = document.createElement('div');
+        col2.className = 'col-6';
+        col2.style.display = 'flex';
+        col2.style.alignItems = 'center';
+        col2.style.justifyContent = 'center';
+        
         let h2 = document.createElement('h2');
         h2.textContent = name;
 
@@ -37,14 +79,23 @@ function displayNamesOnScreen() {
         deleteBtn.addEventListener('click', function () {
             removeFromLocalStorage(name)
             row.remove()
+            nameLength()
         });
 
-        row.appendChild(h2);
-        row.appendChild(deleteBtn);
+        col1.appendChild(h2);
+        col2.appendChild(deleteBtn);
+        row.appendChild(col1);
+        row.appendChild(col2);
         displayNames.appendChild(row);
     })
 }
 displayNamesOnScreen()
+
+function nameLength() {
+    let peopleNames = getLocalStoage()
+    nameLengthDisplay.textContent = `Names in list: ${peopleNames.length}`;
+}
+
 
 
 
